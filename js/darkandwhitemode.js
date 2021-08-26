29597dccfdc6b7e175e86f448221d2e8c1af84e2
jorsuap <jorsuap@gmail.com>
2021-08-26 17:38:25 -0500

    master

--- js/darkandwhitemode.js
+++ js/darkandwhitemode.js
@@ -2,20 +2,20 @@
 let baner = document.getElementsByClassName("fondo")[0];
 let title = document.getElementsByClassName("title")[0];
 let body = document.getElementsByTagName("body")[0];
-let cardsocial = document.querySelectorAll(".socialcard");
+let cardsocial = document.querySelectorAll(".socialcard");//selecion de todos los elementos que contecgan la misma clase
 let cardoverview = document.querySelectorAll(".overiewcards");
 var checkbox = document.getElementsByClassName("toggle")[0];
 let followers = document.querySelectorAll(".socialcard__followers");
 let itemnumber = document.querySelectorAll(".item__number");
 
-checkbox.addEventListener('change', function () {
+checkbox.addEventListener('change', function () {//Validacion de el checkbox
     if(this.checked){
      
         body.style.background = "hsl(0, 0%, 100%)";
         title.style.color = "hsl(230, 17%, 14%)";
         baner.style.background = "hsl(225, 100%, 98%)";
         
-        cardsocial.forEach(elemento =>{
+        cardsocial.forEach(elemento =>{//aplicar a cada elemento de esta variable las entencias siguientes
             // elemento.style.background = "hsl(227, 47%, 96%)";
             elemento.classList.add("light");
             elemento.classList.remove("hoverdark");
